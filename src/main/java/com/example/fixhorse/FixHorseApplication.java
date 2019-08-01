014aec4f5dd6668ecf3330206fb7c8f9b216ad8c
Dmitry Barskov <barskoidim@gmail.com>
2019-08-02 01:32:49 +0300

    initial commit

---
+++ src/main/java/com/example/fixhorse/FixHorseApplication.java
@@ -0,0 +1,12 @@
+package com.example.fixhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixHorseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FixHorseApplication.class, args);
+    }
+}
